7a7447ee326720466a61e086ea4836416cedd1ad
CristianGonzalez24 <crissfcacho@gmail.com>
2023-08-24 12:58:45 -0300

    In progress

--- src/App.jsx
+++ src/App.jsx
@@ -6,35 +6,39 @@ class AutoScalingText extends React.Component {
     state = {
         scale: 1
     }
-};
 
-componentDidUpdate() {
-    const { scale } = this.state
+    componentDidUpdate() {
+        const { scale } = this.state
 
-    const node = this.node
-    const parentNode = node.parentNode
+        const node = this.node
+        const parentNode = node.parentNode
 
-    const availableWidth = parentNode.offsetWidth
-    const actualWidth = node.offsetWidth
-    const actualScale = availableWidth / actualWidth
+        const availableWidth = parentNode.offsetWidth
+        const actualWidth = node.offsetWidth
+        const actualScale = availableWidth / actualWidth
 
-    if (scale === actualScale) return
+        if (scale === actualScale) return
 
-    if (actualScale < 1) {
-        this.setState({ scale: actualScale })
-    } else if (scale < 1) {
-        this.setState({ scale: 1})
+        if (actualScale < 1) {
+            this.setState({ scale: actualScale })
+        } else if (scale < 1) {
+            this.setState({ scale: 1})
+        }
+    }
+
+    render() {
+        const { scale } = this.state
+
+        return (
+            <div
+            className="auto-scaling-text"
+            style={{transform:`scale(${scale}, ${scale})`}}
+            ref={node => this.node = node}
+            >{this.props.children}</div>
+        )
     }
 }
-
-render() {
-    const { scale } = this.state
-
-    return (
-        <div
-        className="auto-scaling-text"
-        style={{transform:`scale(${scale}, ${scale})`}}
-        ref={node => this.node = node}
-        >{this.props.children}</div>
-    )
+class CalculatorDisplay extends React.Component {
+    
 }
+
