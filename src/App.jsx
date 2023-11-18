24cde0926de540c39afe3118bc26dc855672ad18
CristianGonzalez24 <crissfcacho@gmail.com>
2023-11-18 12:39:52 -0300

    In progress

--- src/App.jsx
+++ src/App.jsx
@@ -168,5 +168,6 @@ class Calculator extends React.Component {
 
     performOperation(nextOperator) {
         const { value, displayValue, operator } = this.state
+        const inputValue = parseFloat(displayValue)
     }
 }
