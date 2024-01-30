0f412b2507a1c1e069c211c08a10c36e65443290
CristianGonzalez24 <crissfcacho@gmail.com>
2024-01-30 10:58:49 -0300

    In progress

--- src/App.jsx
+++ src/App.jsx
@@ -240,7 +240,7 @@ class Calculator extends React.Component {
                 <div className='calculator-keypad'>
                     <div className='input-keys'>
                         <div className='function-keys'>
-                            <CalculatorKey className='key-clear' onPress={() => clearDisplay ? this.clearDisplay() : ''}></CalculatorKey>
+                            <CalculatorKey className='key-clear' onPress={() => clearDisplay ? this.clearDisplay() : this.clearAll()}>{clearText}</CalculatorKey>
                         </div>
                     </div>
                 </div>
