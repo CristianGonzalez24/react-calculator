5ab08bad515aedd8d00f2ed1890cbb0fa24f0e1d
CristianGonzalez24 <crissfcacho@gmail.com>
2024-02-15 10:51:02 -0300

    In progress

--- src/App.jsx
+++ src/App.jsx
@@ -245,7 +245,7 @@ class Calculator extends React.Component {
                             <CalculatorKey className="key-percent" onPress={() => this.inputPercent()}>%</CalculatorKey>
                         </div>
                         <div className="digit-keys">
-
+                            <CalculatorKey></CalculatorKey>
                         </div>
                     </div>
                 </div>
