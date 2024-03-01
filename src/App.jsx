3712b44e01e168308c661129fa0bf03d48887fb6
CristianGonzalez24 <crissfcacho@gmail.com>
2024-03-01 11:54:06 -0300

    In progress

--- src/App.jsx
+++ src/App.jsx
@@ -248,7 +248,7 @@ class Calculator extends React.Component {
                             <CalculatorKey className="key-0" onPress={() => this.inputDigit(0)}>0</CalculatorKey>
                             <CalculatorKey className="key-dot" onPress={() => this.inputDot()}>●</CalculatorKey>
                             <CalculatorKey className="key-1" onPress={() => this.inputDigit(1)}>1</CalculatorKey>
-                            <CalculatorKey className="key-2" onPress={() => ''}>2</CalculatorKey>
+                            <CalculatorKey className="key-2" onPress={() => this.inputDigit(2)}>2</CalculatorKey>
                         </div>
                     </div>
                 </div>
